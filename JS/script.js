13e94caf57f874e577996daae07982928404b720
Francis <fransterx123@gmail.com>
2017-02-14 18:29:35 -0500

    Added javascript functionality

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,23 @@
+/*
+Author: Francis Keith Mangornong
+CS 1520 Web Programming Languages
+Spring 2017
+
+Assignment 2 -- scripting
+*/
 
 function checkInput() {
 
-  //to do
-  
+  var message = documents.forms["submissions"]["message"].value;
+
+  var text = "";
+
+  if(message == "") {
+    text.concat("Please include a message. \n");
+  }
+  else {
+    text.concat("Thanks for submitting a message, I'll get back to you soon! ");
+  }
+  document.getElementById('status').innerHTML = text;
+
 }
