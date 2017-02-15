ce1d59c95ed93b337eabd65b4a15d5bb47565a09
Francis <fransterx123@gmail.com>
2017-02-15 17:56:18 -0500

    Final commit and cleaning up

--- JS/script.js
+++ JS/script.js
@@ -3,33 +3,34 @@
   CS 1520 Web Programming Languages
   Spring 2017
 
-  Assignment 2 -- scripting
+  Assignment 2 -- scripting and form functionality
 */
 
 
 /*
   Validate form by checking if fields have been filled out
-  ...Not too crazy on the checking...As long as the field is filled out it's ok
+  Not too crazy on the checking...As long as the field is filled out it's ok
 */
 function validateForm() {
 
+  //  Used to store user input
   var name      =    document.forms["myform"]["name"].value,
       email     =    document.forms["myform"]["email"].value,
       message   =    document.forms["myform"]["message"].value;
 
-  //  Used to for error checking
+  //  Used for error checking
   var array   =   [name, email, message],
       fields  =   ["name", "email", "message"];
 
-  //  Used for error printing
+  //  Used for returning error message
   var err_msg     =   "",
       err_disp    =   "",
       err_count   =   0;
 
   //  Check to make sure all fields are filled out
   for (var i = 0; i < array.length; i++) {
-    if(array[i] == "" || array[i] == null) {
-      err_msg = err_msg + "Your " + fields[i] + " is missing. \n"; // displays as a pop-up alert 
+    if(array[i] == "" || array[i] == null) { // if not filled out build error string to return to user
+      err_msg = err_msg + "Your " + fields[i] + " is missing. \n"; // displays as a pop-up alert
       err_disp = err_disp + "Your " + fields[i] + " is missing. <br>"; // displays on screen
       err_disp = err_disp.fontcolor("red"); // display the error message in red
       err_count++;
@@ -41,10 +42,10 @@ function validateForm() {
       alert(err_msg);
       document.getElementById("formStatus").innerHTML = err_disp;
   }
-  else {
+  else { // no errors
     alert("Thanks " + name + " for your message! \n I'll respond shortly (but not really) at your email " + email);
-    document.getElementById("formStatus").innerHTML = "Thanks for submitting a message! ";
-    document.getElementById("SectionThree").style.display = 'none'; // hide form;
+    document.getElementById("formStatus").innerHTML = "<br>Thanks for submitting a message! <br><br> If you would like to submit another message please refresh the page";
+    document.getElementById("form").style.display = 'none'; // hide form;
   }
 
   return false;
