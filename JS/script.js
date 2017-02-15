92d6d235ec79ceccc0f18f766bb5e7a453ddb9d9
Francis <fransterx123@gmail.com>
2017-02-14 20:59:54 -0500

    Added documentation

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,15 @@
 /*
-Author: Francis Keith Mangornong
-CS 1520 Web Programming Languages
-Spring 2017
+  Author: Francis Keith Mangornong
+  CS 1520 Web Programming Languages
+  Spring 2017
 
-Assignment 2 -- scripting
+  Assignment 2 -- scripting
 */
 
 
 /*
-Validate form by checking if fields have been filled out
-...Not too crazy on the checking...As long as the field is filled out it's ok
+  Validate form by checking if fields have been filled out
+  ...Not too crazy on the checking...As long as the field is filled out it's ok
 */
 function validateForm() {
 
@@ -29,7 +29,7 @@ function validateForm() {
   //  Check to make sure all fields are filled out
   for (var i = 0; i < array.length; i++) {
     if(array[i] == "" || array[i] == null) {
-      err_msg = err_msg + "Your " + fields[i] + " is missing. \n"; // displays as an alert pop-up
+      err_msg = err_msg + "Your " + fields[i] + " is missing. \n"; // displays as a pop-up alert 
       err_disp = err_disp + "Your " + fields[i] + " is missing. <br>"; // displays on screen
       err_disp = err_disp.fontcolor("red"); // display the error message in red
       err_count++;
@@ -43,7 +43,7 @@ function validateForm() {
   }
   else {
     alert("Thanks " + name + " for your message! \n I'll respond shortly (but not really) at your email " + email);
-    document.getElementById("formStatus").innerHTML = "Thanks for submitting a message! "
+    document.getElementById("formStatus").innerHTML = "Thanks for submitting a message! ";
     document.getElementById("SectionThree").style.display = 'none'; // hide form;
   }
 
